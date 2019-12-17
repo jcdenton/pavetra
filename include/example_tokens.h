a12c29f63e776571d6f24df4f322e5eab2f52327
jcdenton <jcdenton@users.noreply.github.com>
2019-12-17 21:19:03 +0300

    Added InfluxDB 2 support

---
+++ include/example_tokens.h
@@ -0,0 +1,13 @@
+#define PAVETRA_WIFI_SSID "Pavetra"
+#define PAVETRA_WIFI_PASSWORD ""
+
+#define PAVETRA_ENDPOINT "https://pavetra.online/devices/data"
+#define PAVETRA_TOKEN ""
+#define PAVETRA_HTTPS_FINGERPRINT "F5 1B 7D A0 A5 8E 9E 07 9E 9F 7E F9 0E 0E 8A BE 57 D1 9A 5A"
+
+#define INFLUX_HOST "eu-central-1-1.aws.cloud2.influxdata.com"
+#define INFLUX_PORT 443
+#define INFLUX_HTTPS_FINGERPRINT "54 5A 5E DA C6 2A C3 9B 86 F8 73 C9 F1 EE AB A2 B3 9B B2 FC"
+#define INFLUX_TOKEN ""
+#define INFLUX_ORG ""
+#define INFLUX_BUCKET ""
--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,147 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <InfluxDb.h>
+#include <PMS.h>
+#include <RemoteDebug.h>
+#include <SoftwareSerial.h>
 #include <WiFiManager.h>
 
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#pragma GCC diagnostic ignored "-Wformat"
+#pragma GCC diagnostic ignored "-Wwrite-strings"
 
-#include "PMS.h"
+// #define DEBUG_PMS
+#define SLEEP_INTERVAL 15 * 60 * 1000
+#define SLEEP_INTERVAL_MICROS SLEEP_INTERVAL * 1000
+
+#include <tokens.h>
+#if !defined(PAVETRA_TOKEN) || !defined(INFLUX_TOKEN)
+#pragma GCC error "Redefine the required constants in your own <tokens.h>"
+#endif
 
 int pm25 = 0;
 int pm10 = 0;
 String pm_data;
 
-PMS pms(Serial);
+RemoteDebug Debug;
+SoftwareSerial pmsSerial(D6, D7);
+PMS pms(pmsSerial);
 PMS::DATA data;
 
+void wakeUpPMS();
+void readDataFromPMS(bool retry = true);
+void sendDataToPavetra();
+void sendDataToInfluxDb();
+void printData();
+
 void setup() {
-  Serial.begin(9600);
-  // === Connect to Internet ===
+  Serial.begin(74880);
+  pmsSerial.begin(PMS::BAUD_RATE);
+#ifdef DEBUG_PMS
+  Debug.begin("0.0.0.0");
+  Debug.setSerialEnabled(true);
+#endif
+
   WiFiManager wifiManager;
+#ifndef DEBUG_PMS
+  wifiManager.setDebugOutput(false);
+#endif
   wifiManager.setConfigPortalTimeout(120);
   wifiManager.setTimeout(120);
-  wifiManager.autoConnect("pavetra");
-
-  // === Get PM data ===
-  pms.wakeUp();
+  wifiManager.autoConnect(PAVETRA_WIFI_SSID, PAVETRA_WIFI_PASSWORD);
 }
 
 void loop() {
-  delay(30*1000);
+  Debug.handle();
+  wakeUpPMS();
   readDataFromPMS();
-  sendDataToServer();
-  // === Sleep Mode ===
+#ifndef DEBUG_PMS
+  sendDataToPavetra();
+  sendDataToInfluxDb();
+#endif
+
   pms.sleep();
-  ESP.deepSleep(20*60*1000*1000); // Sleep 20 minutes
+#ifdef DEBUG_PMS
+  delay(5 * 1000);
+#else
+  ESP.deepSleep(SLEEP_INTERVAL_MICROS);
+#endif
 }
 
-void readDataFromPMS() {
+void wakeUpPMS() {
+  Debug.printf("%us\tWaking up PMS\n", millis() / 1000);
+  pms.wakeUp();
+  Debug.printf("%us\tWarming up PMS for 30s\n", millis() / 1000);
+  delay(PMS::STEADY_RESPONSE_TIME);
+}
+
+void printData() {
+  Serial.println(pm25 / 10.0);
+  Serial.println(pm10 / 10.0);
+}
+
+void readDataFromPMS(bool retry) {
+  pm25 = 0;
+  pm10 = 0;
   int count_read = 0;
   int count_try_to_read = 0;
   while (count_read < 10 && count_try_to_read < 10) {
-    if (pms.readUntil(data)) {
+    if (pms.readUntil(data, PMS::TOTAL_RESPONSE_TIME)) {
       pm25 += data.PM_AE_UG_2_5;
       pm10 += data.PM_AE_UG_10_0;
       ++count_read;
+      if (data.PM_AE_UG_2_5 == 0 || data.PM_AE_UG_10_0 == 0) {
+        Debug.printf("%us\tread ZERO (%d/%d): %d %d\n", millis() / 1000, count_read, count_try_to_read, data.PM_AE_UG_2_5, data.PM_AE_UG_10_0);
+      } else {
+        Debug.printf("%us\tread OK (%d/%d): %d %d\n", millis() / 1000, count_read, count_try_to_read, data.PM_AE_UG_2_5, data.PM_AE_UG_10_0);
+      }
     } else {
       ++count_try_to_read;
+      Debug.printf("%us\treadUntil timed out (%d/%d)\n", millis() / 1000, count_read, count_try_to_read);
     }
   }
-  pm_data = "{\"sensor_2_5\": " + String(pm25 / 10.0) + ", \"sensor_10\": " + String(pm10 / 10.0) + " }";
-  Serial.println(pm_data);
+  if (count_read != 0) {
+    pm_data = "{\"sensor_2_5\": " + String(static_cast<float>(pm25) / count_read) + ", \"sensor_10\": " + String(static_cast<float>(pm10) / count_read) + " }";
+    Debug.printf("%us\t%s\n", millis() / 1000, pm_data.c_str());
+  } else {
+    pm_data = "{}";
+    if (retry) {
+      wakeUpPMS();
+      readDataFromPMS(false);
+    }
+  }
 }
 
-void sendDataToServer() {
+void sendDataToPavetra() {
   // === HTTPS Request ===
   HTTPClient http;
-  http.begin("https://pavetra.online/devices/data", "F5 1B 7D A0 A5 8E 9E 07 9E 9F 7E F9 0E 0E 8A BE 57 D1 9A 5A");
-  http.addHeader("Authorization", "Token ***");
+  http.begin(PAVETRA_ENDPOINT, PAVETRA_HTTPS_FINGERPRINT);
+  http.addHeader("Authorization", "Token " PAVETRA_TOKEN);
   http.addHeader("Content-Type", "application/json");
   int httpCode = http.POST(pm_data);
   http.end();
+}
+
+void sendDataToInfluxDb() {
+  Influxdb influx(INFLUX_HOST, INFLUX_PORT);
+#ifdef INFLUX_HTTPS_FINGERPRINT
+  influx.setFingerPrint(INFLUX_HTTPS_FINGERPRINT);
+#endif
+  influx.setToken(INFLUX_TOKEN);
+  influx.setVersion(2);
+  influx.setOrg(INFLUX_ORG);
+  influx.setBucket(INFLUX_BUCKET);
+
+  InfluxData pm25Measurement("pm25");
+  pm25Measurement.addTag("location", "k34b");
+  pm25Measurement.addTag("sensor", "pms7003");
+  pm25Measurement.addValue("value", pm25 / 10.0);
+  influx.prepare(pm25Measurement);
+
+  InfluxData pm10Measurement("pm10");
+  pm10Measurement.addTag("location", "k34b");
+  pm10Measurement.addTag("sensor", "pms7003");
+  pm10Measurement.addValue("value", pm10 / 10.0);
+  influx.prepare(pm10Measurement);
+
+  influx.write();
 }
